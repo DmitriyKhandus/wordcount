b768f0eedbc646aa13be7d0c4df6cd6a4f5bb5c8
Дмитрий Хандусь <dkhandus@MacBook-Air-Dmitrij.local>
2022-12-07 21:30:06 +0300

    tst5

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("0")
 		return
+	} else if os.Args[1] == " " {
+		fmt.Println("0")
+		return
 	}
 	fmt.Println(len(strings.Split(os.Args[1], " ")))
 }
